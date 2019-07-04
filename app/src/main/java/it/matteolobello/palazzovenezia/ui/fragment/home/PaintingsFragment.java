30f920e3841383fbe3eceefbb70cb0665936d9c2
Matteo Lobello <matteolob1704@gmail.com>
2019-07-04 03:19:01 +0200

    Show Title bar after scrolling

--- app/src/main/java/it/matteolobello/palazzovenezia/ui/fragment/home/PaintingsFragment.java
+++ app/src/main/java/it/matteolobello/palazzovenezia/ui/fragment/home/PaintingsFragment.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -21,10 +20,12 @@ import it.matteolobello.palazzovenezia.data.bundle.BundleKeys;
 import it.matteolobello.palazzovenezia.data.model.Painting;
 import it.matteolobello.palazzovenezia.ui.adapter.recyclerview.PaintingsRecyclerViewAdapter;
 import it.matteolobello.palazzovenezia.ui.view.recyclerview.PaintingsRecyclerViewItemDecoration;
+import it.matteolobello.palazzovenezia.util.DpPxUtil;
 
 public class PaintingsFragment extends Fragment {
 
     private NestedScrollView mNestedScrollView;
+    private View mToolbarView;
     private TextView mTitleTextView;
     private RecyclerView mRecyclerView;
 
@@ -39,6 +40,7 @@ public class PaintingsFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         mNestedScrollView = view.findViewById(R.id.nested_scroll_view);
+        mToolbarView = view.findViewById(R.id.toolbar);
         mTitleTextView = view.findViewById(R.id.paintings_title_text_view);
         mRecyclerView = view.findViewById(R.id.paintings_recycler_view);
 
@@ -54,14 +56,45 @@ public class PaintingsFragment extends Fragment {
                 activity.getIntent().<Painting>getParcelableArrayListExtra(BundleKeys.EXTRA_ALL_PAINTINGS)));
         mRecyclerView.addItemDecoration(new PaintingsRecyclerViewItemDecoration());
 
-        final int titleMarginTop = ((LinearLayout.LayoutParams) mTitleTextView.getLayoutParams()).topMargin;
+        mToolbarView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mNestedScrollView.smoothScrollTo(0, 0);
+            }
+        });
+
         mNestedScrollView.getViewTreeObserver().addOnScrollChangedListener(new ViewTreeObserver.OnScrollChangedListener() {
+
+            boolean isToolbarVisible = false;
+
             @Override
             public void onScrollChanged() {
-                int scrollY = mNestedScrollView.getScrollY();
-                float percentage = scrollY == 0 ? 0 : scrollY * 100 / titleMarginTop;
+                float scrollY = mNestedScrollView.getScrollY();
+                float percentage = scrollY == 0 ? 0 :
+                        scrollY * 100 / (mTitleTextView.getHeight() - DpPxUtil.convertDpToPixel(16));
+                float newAlpha = 1 - (percentage / 100);
 
-                mTitleTextView.setAlpha(1 - (percentage / 100));
+                if (newAlpha <= 0) {
+                    if (!isToolbarVisible) {
+                        mToolbarView.animate()
+                                .translationY(0)
+                                .alpha(1.0f)
+                                .setDuration(200)
+                                .start();
+
+                        isToolbarVisible = true;
+                    }
+                } else {
+                    if (isToolbarVisible) {
+                        mToolbarView.animate()
+                                .translationY(-mToolbarView.getHeight())
+                                .alpha(0.0f)
+                                .setDuration(200)
+                                .start();
+
+                        isToolbarVisible = false;
+                    }
+                }
             }
         });
 
